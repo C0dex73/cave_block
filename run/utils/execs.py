17e1706635ff9b05a7f12a8399d9c0ea7b35d6a2
C0dex73 <c0d3x73@gmail.com>
2022-11-24 15:21:49 +0100

    FINALY solve the exec problem

--- run/utils/execs.py
+++ run/utils/execs.py
@@ -1,13 +1,20 @@
-class DataExecs:
-    def tranform(strCommand, Data):
-        strCommand = strCommand.replace("'", '"')
-        start = 0
-        command = ""
-        while "${" in strCommand[start:len(strCommand)+1]:
-            args = strCommand[strCommand.find("${", start)+2:strCommand.find("}", start)]
-            print(args)
-            command += strCommand[start:strCommand.find("${", start)] + str(Data[strCommand[strCommand.find("${", start)+2:strCommand.find("}", start)]])
-            start = strCommand.find("}", start+1)
-        print(strCommand[start])
-        command += strCommand[start:len(strCommand)+1]
-        return command
+#use to execute commands with arguments relatively to the objects
+#called by many scripts
+
+class DataExecs: #for Data objects [json files]
+    def tranform(strCommand, Data): #transform string into executable commands
+        command = "" #the final command (we'll add substrings to this)
+        i = 0
+        while i < len(strCommand): #for all the characters in strCommand
+            if strCommand[i] == "$" and strCommand[i+1] == "{": #if it's the start of an arg ('${')
+                arg = strCommand[i+2:strCommand.find("}", i+2)] #take the arg
+                arg.replace("'", '"') #replace the ' in json to " in python for the syntax
+
+                command += str(Data[arg]) #add the value of the arg to the command
+                i = strCommand.find("}", i+2) #set the search to after the arg
+            else :
+                command += strCommand[i] #add the char
+            i += 1 #increment i
+        print(strCommand[i-1])
+        print(command)
+        return command #return the command for the execution of it
