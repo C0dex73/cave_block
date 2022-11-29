0bf28e6352c61df2a5535ea4ceec438d3914e40a
C0dex73 <c0d3x73@gmail.com>
2022-11-29 15:20:13 +0100

    actualisation du md

--- main.py
+++ main.py
@@ -5,30 +5,27 @@ import random
 
 def main():
     pygame.init() #initialize pygame
-    running = -1 # running variable
+    state = -1 # state variable
     clock = pygame.time.Clock()
-    while running != 0: #main loop
+    while state != 0: #main loop
         for event in pygame.event.get(): #get all the pygame events
 
             if event.type == pygame.QUIT: #if the user close the window
-                running = 0 #stop the main loop
+                state = 0 #stop the main loop
             
-            if running == -1: #first time running
+            if state == -1: #first time state
                 screen = pygame.display.set_mode((400, 400)) #set the screen to 400 by 400 pixels
                 pygame.display.set_caption("CaveBlock") #set the title of the window
                 initMenu(screen)
                 #TODO : add init stuff here
-                running = 1
+                state = 1
             
-            if running == 1:
+            if state == 1:
                 pygame.display.update() #update the screen
                 #TODO : add tick stuff here
             clock.tick(60) #set the clock rate to 60 fps
 
     pygame.quit() #stop the program
-
-def initMenu(screen):
-    
 
 
 if __name__ == "__main__":
--- project_architecture.md
+++ project_architecture.md
@@ -9,3 +9,13 @@ MENU (first window):
                     LOAD button
                     NEW button
                     OPTIONS button
+
+fonctionnement :
+
+Lancement du jeu,
+initialisation de la boucle, de l'écran et de ses propriété
+initialisation du menu
+state = 0
+mainloop :
+    event checking
+    appel de la classe courante (args : [screen, inputs]) (utilisation d'une fonction tick() ?) {va dépendre de state (0:null, 1:menu, 2:game, 3:gameOverScreen, 4:Intro)}
