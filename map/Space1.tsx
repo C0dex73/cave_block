994205b2d88114de437d6957c171ed2d0fcce253
Corroder <coroder22@gmail.com>
2022-11-29 14:54:57 +0100

    test des Tileset

---
+++ map/Space1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Space1" tilewidth="32" tileheight="32" tilecount="35" columns="7">
+ <image source="../Tileset.png" width="240" height="176"/>
+</tileset>
