074e9a45b5fdea01d1c2ceb121bb43ec987a2982
C0dex73 <c0d3x73@gmail.com>
2022-11-29 13:46:53 +0100

    Remove usless stuff

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-#========== MAIN FILE ==========
-#
-# called by the user
-# call the menu file and the useJSON file
-#
+
 import pygame
 import math
 import random
-from run.menu import Menu
-from data.useJSON import useJSON
-
 pygame.init() #initialize pygame
 
-#using useJSON.Get("data/app.json") to get the app dictionary
-screen = pygame.display.set_mode(useJSON.Get("data/app.json")["size"]) #set the screen to 400 by 400 pixels
-pygame.display.set_caption(useJSON.Get("data/app.json")["title"])
-
 running = -1 # running variable
-
+clock = pygame.time.Clock()
 while running != 0: #main loop
     for event in pygame.event.get(): #get all the pygame events
 
         if event.type == pygame.QUIT: #if the user close the window
             running = 0 #stop the main loop
-
-        if running == -1: #if it's the first iteration
-            Menu(screen) #initialize the menu (run Menu.__init__(Menu, screen))
-            running = 1 #avoid a recall of this section
         
-        pygame.display.flip() #update the screen
+        if running == -1:
+            screen = pygame.display.set_mode((400, 400)) #set the screen to 400 by 400 pixels
+            pygame.display.set_caption("CaveBlock") #set the title of the window
+            #TODO : add init stuff here
+            running = 1
+        
+        if running == 1:
+            pygame.display.update() #update the screen
+            #TODO : add tick stuff here
+        clock.tick(60) #set the clock rate to 60 fps
 
 pygame.quit() #stop the program
+
+class Menu:
+    def innit(screen):
+        image = pygame.image.load("")
