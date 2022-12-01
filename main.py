a926ce58c6a98cf2bd3bb91c4d1201ac3ea8c82f
C0dex73 <c0d3x73@gmail.com>
2022-12-01 20:58:26 +0100

    null

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pygame
 import math
 import random
@@ -15,54 +14,91 @@ def main():
     pygame.display.set_caption(Data["screen"]["caption"]) #set the title of the window
 
     while state != 0: #main loop
-        for event in pygame.event.get(): #get all the pygame events
-
+        events = pygame.event.get() #get all the pygame events and store them in a variable
+        for event in events:
             if event.type == pygame.QUIT: #if the user close the window
                 state = 0 #stop the main loop
-            
-        #TODO : add tick stuff here
 
-        scene.tick(screen)
-        if scene.next == None: #TODO : add stop from next
-            state
-        scene = scene.next #change to the next scene defined by the actual scene
-        pygame.display.update()
-        clock.tick(60) #set the clock rate to 60 fps
+        scene.tick(screen, events) #do everything to do in the tick of the scene
+        if scene.next == None: #if we don't have any scene next
+            state = 0 #stop the main loop
+        else :
+            scene = scene.next #change to the next scene defined by the actual scene
+        pygame.display.update() #update the screen
+        clock.tick(120) #set the clock rate to 60 fps
     pygame.quit() #stop the program
 
 #classes
 class Menu: #to handle the menu display
-    def __init__(self, screen):
+    def __init__(self, screen): #called while initializing the class
         self.next = self #set the next scene to itself
         self.font = pygame.font.Font("textures/font.ttf", 100) #set the font
 
-        #parts of tick function for if detection
-        self.text = self.font.render("EXIT", True, (250, 250, 250))
-    def tick(self, screen): #called every game tick
-
+        #parts of tick function to avoid errors with self call
+        self.ExitText = self.font.render("Exit", True, (250, 250, 250)) #set the text of the ExitButton
+        self.OptionText = self.font.render("Option", True, (250, 250, 250)) #set the text of the OptionButton
+        
+    
+    
+    def tick(self, screen, events): #called every active tick
         #background image
         self.bg = pygame.image.load("textures/test.png").convert() #load the bg menu image
         self.bg = pygame.transform.scale(self.bg, Data["screen"]["size"]) #rescale the image to the size of the screen
         screen.blit(self.bg, (0, 0)) #print the image on the screen
-
         #EXIT button
-        exitButtonTLCorner = (50, 600)
-        self.exitButtonSurface = self.text.get_rect(topleft=exitButtonTLCorner)
-        if self.exitButtonSurface.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
-            self.text = self.font.render("EXIT", True, (250, 250, 250))
-            if testEvent(pygame.MOUSEBUTTONDOWN): #TODO : add quit()
-                
-        else:
-            self.text = self.font.render("EXIT", True, (200, 200, 200))
-        screen.blit(self.text, exitButtonTLCorner)
-        
+        exitButtonTLCorner = (50, 600) #set the topleft corner of the button
+        self.exitButtonSurface = self.ExitText.get_rect(topleft=exitButtonTLCorner) #get the whole text surface
+        if self.exitButtonSurface.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]): #if the mouse hoover the text
+            self.ExitText = self.font.render("Exit", True, (250, 250, 250)) #make it lighter
+            if testEvent([pygame.MOUSEBUTTONDOWN], events): #if the user click
+                self.next = None #stop the game (state = 0 in mainloop)
+        else: 
+            self.ExitText = self.font.render("Exit", True, (200, 200, 200)) #set the text to his normal color
+        screen.blit(self.ExitText, exitButtonTLCorner) #render the text
+
+        #OPTION button
+        optionButtonTLCorner = (50, 500) #set the topleft corner of the button
+        self.optionButtonSurface = self.OptionText.get_rect(topleft=optionButtonTLCorner) #get the whole text surface
+        if self.optionButtonSurface.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]): #if the mouse hoover the text
+            self.OptionText = self.font.render("Option", True, (250, 250, 250)) #make it lighter
+            if testEvent([pygame.MOUSEBUTTONDOWN], events): #if the user click
+                self.next = Options(screen) #pass to Option scene (state = 2 in mainloop)
+        else: 
+            self.OptionText = self.font.render("Option", True, (200, 200, 200)) #set the text to his normal color
+        screen.blit(self.OptionText, optionButtonTLCorner) #render the text
+
+class Options: #to handle the option menu display
+    def __init__(self, screen): #called when initializing this class
         self.next = self #set the next scene to itself
+        self.font = pygame.font.Font("textures/font.ttf", 100) #set the font
+
+        #parts of tick function to avoid errors with self call
+        self.MenuText = self.font.render("Menu", True, (250, 250, 250)) #set the text of the ExitButton
+
+    def tick(self, screen, events): #called every active tick
+        #background image
+        self.bg = pygame.image.load("textures/used/bigdoor23.png").convert() #load the bg option image #TODO : replace this with the real path
+        self.bg = pygame.transform.scale(self.bg, Data["screen"]["size"]) #rescale the image to the size of the screen
+        screen.blit(self.bg, (0, 0)) #print the image on the screen
+
+        #OPTION button
+        menuButtonTLCorner = (50, 600) #set the topleft corner of the button
+        self.menuButtonSurface = self.MenuText.get_rect(topleft=menuButtonTLCorner) #get the whole text surface
+        if self.menuButtonSurface.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]): #if the mouse hoover the text
+            self.MenuText = self.font.render("Menu", True, (250, 250, 250)) #make it lighter
+            if testEvent([pygame.MOUSEBUTTONDOWN], events): #if the user click
+                self.next = Menu(screen) #pass to Option scene (state = 2 in mainloop)
+        else: 
+            self.MenuText = self.font.render("Menu", True, (200, 200, 200)) #set the text to his normal color
+        screen.blit(self.MenuText, menuButtonTLCorner) #render the text
 
 
-def testEvent(event):
-    for e in pygame.event.get():
-        if e.type == event:
-            return True
+
+def testEvent(Tevents, Revents):
+    for event in Tevents: #for each event to check
+        for e in Revents: #for each event appenning
+            if e.type == event: #if there are the event to check return true else return false
+                return True
     return False
 
 #main program
