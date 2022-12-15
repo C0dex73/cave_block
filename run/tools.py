b0dce84a8db72402cabf88829f0db9c3c14695e9
C0dex73 <c0d3x73@gmail.com>
2022-12-15 16:03:17 +0100

    null

--- run/tools.py
+++ run/tools.py
@@ -24,7 +24,12 @@ def testEvent(Tevents, Revents):
         returnVar.append({str(event) : False})
     return returnVar
 
-def TerrainGen(path=None):
+def TerrainGen():
+    #code the terrain by block with symbols
+    #differents calcs is represented by differents symbols
+    #there can be multiple calcs with a transparent background for example
+    #there respective code will be separated by a ;
+    
     terrain = [["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","0", "0"],
                ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","0", "0"],
                ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","0", "0"],
@@ -48,50 +53,46 @@ def TerrainGen(path=None):
                ]
     
     bit = 0
-    theme = random.choice(["2", "3"])
+    theme = random.choice(["2", "3"]) #theme id (see app.json file)
     
     for line in range(len(terrain)):
-        for column in range(len(terrain[line])):
+        for column in range(len(terrain[line])): #for each block
             
+            #if its a border block 
             if column == 0 or column == len(terrain[line])-1 or line == 0 or line == len(terrain)-1:
-                bit += random.randint(-1, 1)                    
+                bit += random.randint(-1, 1) #add or remove 1 to the lenght of the borders                   
                 if bit == -1 :
                     bit = 0
                 if bit >= len(terrain)/4 :
                     bit = int(len(terrain)/4-1)
-                terrain[line][column] = "1"
+                terrain[line][column] = "1" #set the border
             
-            if column == 0:
+            if column == 0: #if it's the left border then add the border lenght
                 for i in range(column, column + bit+1):
                     terrain[line][i] = "1"
                         
-            if column == len(terrain[line])-1:
+            if column == len(terrain[line])-1: #if it's the right border then add the border lenght
                 for i in range(column - bit+1, column):
                     terrain[line][i] = "1"
                         
-            if line == 0:
+            if line == 0: #if it's the roof then add the border lenght
                 for i in range(line, line + bit+1):
                     terrain[i][column] = "1"
-                if random.randint(0, 7) == 0 :
+                if random.randint(0, 4) == 0 : #and might add a cable or a vine
                     terrain[line + bit + 1][column] = "0;*"
                         
-            if line == len(terrain)-1 :
+            if line == len(terrain)-1 : #if it's the ground then add the border lenght
                 for i in range(line - bit+1, line):
                     terrain[i][column] = "1"
                 
-            if terrain[line][column] == "0":
-                percent = random.randint(1, 100)
-                if percent <= 5:
+            if terrain[line][column] == "0": #if its the background
+                percent = random.randint(1, 100) #set percent as a variable wich will define the type of the block
+                if percent <= 5: #5% chance to be a neutral vent (no theme)
                     terrain[line][column] = "/"
-                elif percent <= 10:
-                    terrain[line][column] = theme + "a;//"
-                elif percent < 
-                
+                elif percent <= 10: #5% chance to be a theme vent with the assocate background for transparent parts
+                    terrain[line][column] = theme + "a;//" + theme
         
-    if path is not None:
-        json.dump(terrain, open(path, 'w'))
-    else:
-        return terrain
+    return terrain #return the generated terrain
     
 def DrawTerrain(screen, CodedTerrain, Data, saveFilePath=None): #TODO : implement the seed mechanism and the colliders
     finalTerrainSurface = pygame.Surface(Data["screen"]["size"]) #* this work tho
