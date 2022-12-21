925689e83324b2c358c4eb484dc707a2f01ad3aa
C0dex73 <c0d3x73@gmail.com>
2022-12-21 19:26:38 +0100

    add jump and gravity

--- run/entities.py
+++ run/entities.py
@@ -3,7 +3,7 @@ from run.tools import Rescaler
 import time
 import math
 
-class Player(pygame.sprite.Sprite):
+class Player():
     def __init__(self, screen, position, Data):
         self.Data = Data
         self.speed = Data["entities"]["player"]["speed"]
@@ -48,9 +48,12 @@ class Player(pygame.sprite.Sprite):
         
 
     def tick(self, screen, events, keys, terrainCollider):
-        
+
         #bool(Sprite()) will return True if the Sprite exists, we check if the 
         isGrounded = bool(pygame.sprite.spritecollideany(self.checkSprites.sprites()[0], terrainCollider))
+        
+        #gravity
+        if not isGrounded : self.direction.y += 0.1
         
         #draw the image
         playerImage = pygame.image.load("textures/used/Astro_" + str(math.floor(self.imageState)) + ".png").convert_alpha()
@@ -65,47 +68,32 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
         
-        if keys[eval("pygame.K_" + self.Data["inputs"]["jump"])] :
+        if keys[eval("pygame.K_" + self.Data["inputs"]["jump"])] and isGrounded: #to jump the player have to be on the ground
             self.direction.y = -1 * self.speed
-        elif keys[eval("pygame.K_" + self.Data["inputs"]["crouch"])] :
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
+        elif keys[eval("pygame.K_" + self.Data["inputs"]["crouch"])] and isGrounded:
+            self.direction.x /= 2
             
         #rotate the image before direction correction
         if self.direction.x < 0: playerImage = pygame.transform.flip(playerImage, True, False)
         
-        #for each collider detection face
-        for collider in self.checkSprites:
-            if self.Data["devMode"]: #draw it if in dev mode
-                pygame.draw.rect(screen, (255, 0, 0), collider.rect) # type: ignore (rect not static)
-            if pygame.sprite.spritecollideany(collider, terrainCollider): #if there is collision
-                # locate wich face collided with the terrain by checking the distance between the center of the check collider and the center of the player
-                colliderDelta = tuple(map(lambda i, j: i - j, collider.rect.center, self.rect.center)) # type: ignore (rect not static) 
-                #?What are we doing ?
-                #*we check between 2 and -2 for 0 due to the actualisation time, the colliders are a bit late when the player moves
-                #*we also check the direction to be sure the collision is due to this type of movements
-                #!for exemple if the player go right, he can also be colliding with the floor, then he can't jump anymore and that's a bug
-                if colliderDelta[0] <= 2 and colliderDelta[0] >= -2 and colliderDelta[1] < 0 and self.direction.y < 0: #if dx = 0 and dy < 0 then its the top (the y axis is reversed : 0;0 is topleft)
-                    self.direction.y = 0
-                if colliderDelta[0] <= 2 and colliderDelta[0] >= -2 and colliderDelta[1] > 0 and self.direction.y > 0: #if dx = 0 and dy > 0 then its the bottom (the y axis is reversed : 0;0 is topleft)
-                    self.direction.y = 0
-                if colliderDelta[1] <= 2 and colliderDelta[1] >= -2 and colliderDelta[0] > 0 and self.direction.x > 0: #if dx > 0 and dy = 0 then its the 
-                    self.direction.x = 0
-                if colliderDelta[1] <= 2 and colliderDelta[1] >= -2 and colliderDelta[0] < 0 and self.direction.x < 0:
-                    self.direction.x = 0
+        #for each check sprite if the player do a movement to go in the wall, then cancel this movement
+        if pygame.sprite.spritecollideany(self.checkSprites.sprites()[1], terrainCollider) and self.direction.y < 0: self.direction.y = 0
+        if pygame.sprite.spritecollideany(self.checkSprites.sprites()[0], terrainCollider) and self.direction.y > 0: self.direction.y = 0
+        if pygame.sprite.spritecollideany(self.checkSprites.sprites()[2], terrainCollider) and self.direction.x > 0: self.direction.x = 0
+        if pygame.sprite.spritecollideany(self.checkSprites.sprites()[3], terrainCollider) and self.direction.x < 0: self.direction.x = 0
                 
         #then move evrything and update the image
         for collider in self.checkSprites:  
             collider.rect.top += self.direction.y # type: ignore (rect not static)
             collider.rect.left += self.direction.x # type: ignore (rect not static)
-        self.position += self.direction
-        
-        
-        screen.blit(playerImage, self.position)
-        
-        
-        if self.direction.x != 0 and time.time_ns()//10**8 % 10 != self.imageTimer:
+            if self.Data["devMode"]: #draw it if in dev mode
+                pygame.draw.rect(screen, (255, 0, 0), collider.rect) # type: ignore (rect not static)
+        #take the top and left collision checkers to get the topleft corner of the image, so the position of the player
+        self.position = (self.checkSprites.sprites()[3].rect.centerx, self.checkSprites.sprites()[1].rect.centery) # type: ignore (rect not static)
+
+        #change the image of the player relatyvely to the time passed
+        if self.direction.x != 0 and time.time_ns()//10**8 % 10 != self.imageTimer: #same calcul as line 11, and if it's different
             self.imageState += 0.125
             if self.imageState == 5 : self.imageState = 1
         
+        screen.blit(playerImage, self.position)
